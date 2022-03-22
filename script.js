d8862bd1d27f625a924a954d8c62a0843b588781
Yogii <stevenp0294@gmail.com>
2022-03-22 15:22:17 -0400

    Made script to create a grid of a specified size added reset button, but is yet to be connect to script CSS to allow grid to change into one other color. next step: to make function that resets grid and attach it to the reset button.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const grid = document.querySelector('#grid');
+const grid_div = document.getElementById('grid');
+const cell_divs = document.getElementsByClassName('cell');
+const reset = document.getElementById('btn1');
+
+function makeGrid(rowNum){
+    for(i = 0; i < (rowNum * rowNum); i++){
+        let cell = document.createElement('div');
+        grid_div.appendChild(cell).classList = 'cell';
+        cell.addEventListener('mouseenter', () => {
+            cell.classList.toggle('hover');
+        })
+    }
+}
+
+makeGrid(16);
+
+function resetGrid(){
+    //selects each div and un-appends from the grid
+    //prompt user for a new rowNum
+        //loop throught makeGrid again with new rowNum
+}
