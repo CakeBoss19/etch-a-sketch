57f507de7d3476112a6e06293869cd5c2902ca95
Yogii <stevenp0294@gmail.com>
2022-03-23 08:54:52 -0400

    modified code to create grid. should allow for unlimited divs to be created and have it all fit within the confines of the 'grid'. css still needs to be written to make them squares.

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ const grid_div = document.getElementById('grid');
 const cell_divs = document.getElementsByClassName('cell');
 const reset = document.getElementById('btn1');
 
+//review flexbox lessons to allow cells to fit automatically within rows.
+//as well as have rows fit automatically within the given grid space.
+//change display: grid to display: flex;
 function makeGrid(rowNum){
-    for(i = 0; i < (rowNum * rowNum); i++){
-        let cell = document.createElement('div');
-        grid_div.appendChild(cell).classList = 'cell';
-        cell.addEventListener('mouseenter', () => {
-            cell.classList.toggle('hover');
-        })
+    for(r = 0; r < rowNum; r++){
+        let row = document.createElement('div');
+        grid_div.appendChild(row).classList = 'row';
+        for(c = 0; c < rowNum; c++){
+            let cell = document.createElement('div');
+            row.appendChild(cell).classList = 'cell';
+        }
     }
 }
+
+// function makeGrid(rowNum){
+//     for(i = 0; i < (rowNum * rowNum); i++){
+//         let cell = document.createElement('div');
+//         grid_div.appendChild(cell).classList = 'cell';
+//         cell.addEventListener('mouseenter', () => {
+//             cell.classList.toggle('hover');
+//         })
+//     }
+// }
 
 makeGrid(16);
 
